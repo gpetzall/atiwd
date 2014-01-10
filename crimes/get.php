6fbaeea81d5aa3012cedd8762dd6f819563e61a7
Gunnar Petzall <gpetzall@gmail.com>
2014-01-10 20:47:54 +0000

    Fixed what was left on 2.1.1
    Note: Not 1.1.1!

--- crimes/get.php
+++ crimes/get.php
@@ -62,24 +62,6 @@ $doc->formatOutput = true;
 $xml = simplexml_load_file($inputFilename);
 
 
-/*
-
-STILL TO DO!!!
-
-- Add England
-- Add Wales
-
-What to do:
-- Make sure to skip Wales in the loop!
-- Sum up all *regions* except Wales from the DOM that is displayed.
-- Add an england child with the total added up.
-- Add a wales child with the same data as in the region.
-
-*/
-
-
-
-
 switch ($response)
 {
 	case 'xml': // XML block starting.
@@ -97,14 +79,29 @@ switch ($response)
 		$node->setAttribute('year',"6-2013"); // Set year attribute.
 		$crimes = $root->appendChild($node); // Add it to the response.
 		
-	
+		// For later adding england's and wales' totals.
+		$england_total = 0;
+		$wales_total = 0;
+		
 		foreach($xml->children() as $region) // Simple XML for the reading, DOM for writing.
 		{
-			if ($region->attributes()['id'] == 'british_transport_police' || $region->attributes()['id'] == 'action_fraud') // Filtering national.
+			if	( // Filtering national and Wales.
+					$region->attributes()['id'] == 'british_transport_police' ||
+					$region->attributes()['id'] == 'action_fraud' ||
+					$region->attributes()['id'] == 'wales'
+				)
 			{
-				$node = $doc->createElement('national');
-				$node->setAttribute('id',ucwords(str_replace('_', ' ', (string) $region->attributes()['id']))); // ucwords is Very Useful.
-				// Simple XML element with id attribute to string, change characters and add.
+				if ($region->attributes()['id'] == 'wales') // If Wales...
+				{
+					// Do nothing.
+				}
+				else // Otherwise: Make elements!
+				{
+					$node = $doc->createElement('national');
+					$node->setAttribute('id',ucwords(str_replace('_', ' ', (string) $region->attributes()['id']))); // ucwords is Very Useful.
+					// Simple XML element with id attribute to string, change characters and add.
+				} // End Wales-check 1
+				
 				
 				$region_total = 0; // Variable for the region totals.
 				
@@ -119,11 +116,18 @@ switch ($response)
 					} // End crime_type foreach (national).
 				} // End area foreach (national).
 				
-				$node->setAttribute('total',$region_total); // Add the total number.
-				
-				$crimes->appendChild($node); // Add it all to the DOM.
-			} // End if (national).
-			else // (all other)
+				if ($region->attributes()['id'] == 'wales') // If Wales...
+				{
+					$wales_total = $region_total; // Set Wales' total.
+				}
+				else // Otherwise, make a national node.
+				{
+					$node->setAttribute('total',$region_total); // Add the total number.
+					
+					$crimes->appendChild($node); // Add it all to the DOM.
+				} // End Wales-check 2.
+			} // End national if.
+			else // All other regions
 			{
 				$node = $doc->createElement('region');
 				$node->setAttribute('id',ucwords(str_replace('_', ' ', (string) $region->attributes()['id']))); // ucwords is Very Useful.
@@ -145,15 +149,27 @@ switch ($response)
 				$node->setAttribute('total',$region_total); // Add the total number.
 				
 				$crimes->appendChild($node); // Add it all to the DOM.
+				
+				$england_total += $region_total;
 		
 			} // End else (all other).
 			
 		} // End XML foreach.
 		
+		// Add the "england" element.
+		$node = $doc->createElement('england');
+		$node->setAttribute('total',$england_total);
+		$crimes->appendChild($node);
+		
+		
+		// Add the "wales" element.
+		$node = $doc->createElement('wales');
+		$node->setAttribute('total',$wales_total);
+		$crimes->appendChild($node);
+		
+		// Add it to the XML response.
 		echo $doc->saveXML();
 		break; // XML block end.
-	
-	
 	
 	case 'json': // JSON block starting.
 		
@@ -163,15 +179,31 @@ switch ($response)
 		$json['response']['timestamp']=time(); // Create the first array item and its child with a name of timestamp and value of time().
 		$json['response']['crimes']['year']='6-2013'; // Create a second child of response with a child named year and value "6-2013".
 		
+		// For later adding england's and wales' totals.
+		$england_total = 0;
+		$wales_total = 0;
+		
 		$region_count = 0; // To make the loop make new children instead of overwriting itself.
 		
 		foreach($xml->children() as $region) // Similar to the XML loop.
 		{
-			if ($region->attributes()['id'] == 'british_transport_police' || $region->attributes()['id'] == 'action_fraud') // Filtering national.
+			if	( // Filtering national and Wales.
+					$region->attributes()['id'] == 'british_transport_police' ||
+					$region->attributes()['id'] == 'action_fraud' ||
+					$region->attributes()['id'] == 'wales'
+				)
 			{
-				$json['response']['crimes']['national'][$region_count]['id']=(string)ucwords(str_replace('_', ' ',$region->attributes()['id'])); // ucwords is Very Useful.
-				// Create region; put multiple numbered entries in region; which each hold one "id" that is the name of the XML-attribute pulled (same way as XML code).
 				
+				if ($region->attributes()['id'] == 'wales') // If Wales...
+				{
+					// Do nothing.
+				}
+				else
+				{
+					$json['response']['crimes']['national'][$region_count]['id']=(string)ucwords(str_replace('_', ' ',$region->attributes()['id'])); // ucwords is Very Useful.
+					// Create region; put multiple numbered entries in region; which each hold one "id" that is the name of the XML-attribute pulled (same way as XML code).
+				}// End Wales-check 1.
+					
 				$region_total = 0; // Loop identical to XML loop.
 				
 				foreach($region->children() as $area) // Continue down the rabbit hole...
@@ -185,7 +217,14 @@ switch ($response)
 					} // End crime_type foreach (national).
 				} // End area foreach (national).
 				
-				$json['response']['crimes']['national'][$region_count]['total']=(int) $region_total; // Correct data type.
+				if ($region->attributes()['id'] == 'wales') // If Wales...
+				{
+					$wales_total = $region_total; // Set Wales' total.
+				}
+				else // Otherwise, make a national node.
+				{
+					$json['response']['crimes']['national'][$region_count]['total']=(int) $region_total; // Correct data type.
+				} // End Wales-check 2.
 			} // End if (national)
 			else // All other.
 			{
@@ -206,11 +245,19 @@ switch ($response)
 				} // End area foreach (all other).
 				
 				$json['response']['crimes']['region'][$region_count]['total']=(int) $region_total; // Correct data type.
+				
+				$england_total += $region_total;
+				
+				
 			} // End else (all other).
 			
 			$region_count++; // Keeping track of location in array.
 			
 		} // End JSON foreach.
+		
+		// Add the "england" and "wales" elements.
+		$json['response']['crimes']['england']['total']=$england_total;
+		$json['response']['crimes']['wales']['total']=$wales_total;
 		
 		echo json_encode($json); //Json!
 		
