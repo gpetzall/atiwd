e549e3ac0825a32b95c9d78c3c0379ab07fd1bb6
Gunnar Petzall <gpetzall@gmail.com>
2014-01-16 17:28:53 +0000

    Created 2.2.3 introduction

--- crimes/delete.php
+++ crimes/delete.php
@@ -1,31 +1,37 @@
 <?php
 /*
  * Author: Gunnar Petzall (UWE no: 10005826) (gpetzall@gmail.com)
- * Created: 2014-01-10
- * Modified: 2014-01-15
+ * Created: 2014-01-15
+ * Modified: 2014-01-16
  * 
  * Script made for the Advanced Topics in Web Development (UFCEWT-20-3) at the
  * University of the West of England in the years 2013-2014. This is part B1 course
- * component, dealing with part "2.2.2".
+ * component, dealing with part "2.2.3".
  * 
- * The script creates a new *area* within a specified *region* with data for three
- * types of crimes (Homicide, Violence with injury, Violence without injury) in the
- * "crimes.xml" file at http://www.cems.uwe.ac.uk/~g2-petzall/atwd/crimes/doc/
- * and display the new crime totals for each crime, the area and the region.
- * Unspecified data is assumed to be 0.
+ * The script deletes an area (most commonly the prescribed "wessex" one) specified
+ * by HTTP $_GET in the "crimes.xml" file located at
+ * http://www.cems.uwe.ac.uk/~g2-petzall/atwd/crimes/doc/ and display what has been
+ * deleted as well as the new england and england_wales calculated totals.
+ * 
+ * In addition to the assignment specification, this script can delete any area and
+ * can also be specified to one particular region (like if there are duplicates).
+ *
+ * If there is more than one specified area, and the region is not specified, the
+ * first area that matches the specification is deleted.
+ * 
+ * To restore any changes, reset the "crimes.xml" using the "reset.php" script in
+ * this folder.
  * 
  * 
  * Pages used as help to make this code:
  * 
- * Making array with my own key names:
- * http://stackoverflow.com/questions/15716608/create-custom-keys-in-array-with-php
- * [Accessed 2014-01-11]
+ * headline:
  * 
- * How to DELETE a simple XML element (took HOURS!):
- * http://stackoverflow.com/questions/262351/remove-a-child-with-a-specific-attribute-in-simplexml-for-php
- * [Accessed 2014-01-15]
+ * [Accessed 2014-01-16]
  * 
 */
+
+
 
 
 
